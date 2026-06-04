ef253e62d39f49bebeadbc20cd11b00ad85d48b6
FOXYCRON <57571091+FOXYCRON@users.noreply.github.com>
2026-06-03 22:28:35 -0700

    Update ready.js

--- src/events/clients/ready.js
+++ src/events/clients/ready.js
@@ -13,7 +13,7 @@ module.exports = class Ready extends Event {
 			console.log(`(/) ${this.client.slashCommands.size} Comandos Publicados!`.green);
 		}
 		mongoose.set('strictQuery', false);
-		await mongoose.connect('mongodb+srv://dashboard:Irvinadrian5@cluster0.6k029.mongodb.net/').then(() => {
+		await mongoose.connect('mongodb+srv://dashboard:irvinadrian5@cluster0.6k029.mongodb.net/').then(() => {
 			console.log('☁ Conectado a la base de datos de MongoDB');
 		}).catch((err) => {
 			console.log('☁ ERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB');
